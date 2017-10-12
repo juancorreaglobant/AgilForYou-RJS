3c05cf9f7ab077d6194246487fd8c7c5ade5b688
Juan Camilo Correa Torres <juan.correa@globant.com>
2017-10-12 12:26:27 -0500

    Sebas

--- r-master/src/Components/Challenge.js
+++ r-master/src/Components/Challenge.js
@@ -7,127 +7,103 @@ import Chulos from './Chulos';
 class Challenge extends Component {
     constructor(props) {
         super(props);
-        this.sumar = this.sumar.bind(this);
+        this.goNext = this.goNext.bind(this);
+        this.isValid = this.isValid.bind(this);
+
         this.state = {
-            reto: parseInt(this.props.Posicion),
+            reto: 0,
             estado: true,
             reto_1: []
         }
+        this.currentindex = 0;
     }
 
-    sumar() {
-        var javascript = document.getElementById("text_js");
+    isValid(reto) {
+        /*
+            TODO: 
+            1. Hacer las validaciones dependiendo si tiene JS, CSS o HTML. (1hora)
+            2. Mejorar los retos para que incluyan CSS. (10 minutos)
+            3. Subir la informacion al servidor el arreglo con la solucion y la url a la hoja de vida. (1 hora)
+        */
+        if (reto.items.indexOf('js')) {
+            var javascript = document.getElementById("text_js");
+            if (javascript.value) {
+                return false;
+            }
+        }
+       
         var html = document.getElementById("text_html");
         if (this.state.reto === 0 && this.state.estado === true) {
             if (html.value === "" || javascript === "") {
-                alert("TANTO JAVASCRIPT COMO HTML NO PUEDE ESTAR VACIOS");
+                return false;
+
             }
-            else {
-                this.setState({ reto: 1 })
-                var reto = this.state.reto_1;
-                reto["reto1"] = { HTML: html.value, JAVASCRIPT: javascript.value };
-                this.setState({ reto_1: reto });
-                html.value = "";
-                javascript.value = "";
-                console.log(this.state.reto_1);
-            }
+        }
+        return true;
+    }
+
+    goNext() {
+        if (this.isValid(Info[this.state.reto])) {
+            this.setState({ reto: this.currentIndex + 1 })
+            var reto = this.state.reto_1;
+            reto["reto1"] = { HTML: html.value, JAVASCRIPT: javascript.value };
+            this.setState({ reto_1: reto });
+            html.value = "";
+            javascript.value = "";
+            console.log(this.state.reto_1);
         }
         else {
-            if (this.state.reto === 1 && this.state.estado === true) {
-                if (javascript === "") {
-                    alert("EL HTML NO PUEDE ESTAR VACIOS");
-                } else {
-                    this.setState({ reto: 2 })
-                    var reto = this.state.reto_1;
-                    reto["reto2"] = { JAVASCRIPT: javascript.value };
-                    this.setState({ reto_1: reto });
-                    javascript.value = "";
-                    console.log(this.state.reto_1);
-                }
-            }
-            else {
-                if (this.state.reto === 2 && this.state.estado === true) {
-                    if (html.value === "" || javascript === "") {
-                        alert("TANTO JAVASCRIPT COMO HTML NO PUEDE ESTAR VACIOS");
-                    }
-                    else {
-                        this.setState({ reto: 3 })
-                        var reto = this.state.reto_1;
-                        reto["reto3"] = { HTML: html.value, JAVASCRIPT: javascript.value };
-                        this.setState({ reto_1: reto });
-                        html.value = "";
-                        javascript.value = "";
-                        console.log(this.state.reto_1);
-                    }
-                } else {
-                    if (this.state.reto === 3 && this.state.estado === true) {
-                        if (javascript === "") {
-                            alert("EL HTML NO PUEDE ESTAR VACIOS");
-                        } else {
-                            this.setState({ reto: 0 });
-                            this.setState({ estado: false });
-                            var reto = this.state.reto_1;
-                            reto["reto4"] = { JAVASCRIPT: javascript.value };
-                            this.setState({ reto_1: reto });
-                            javascript.value = "";
-                            console.log(this.state.reto_1);
-                        }
-                    }
-                }
-            }
+            alert("TANTO JAVASCRIPT COMO HTML NO PUEDE ESTAR VACIOS");
         }
-    };
-    static propTypes = {
-        Info: PropTypes.array.isRequired
     }
 
 
+
     render() {
-        const { Info, Posicion } = this.props;
+        const { Info } = this.props;
         if (this.state.estado === true) {
             return (
                 <section id="reto3">
                     <div align="center">
                         <h3 className="h" id="titulo3">
-                            {Info[Posicion].titulo}
+                            {Info[this.state.reto].titulo}
                         </h3>
                         <p className="text" id="descripcion3">
-                            {Info[Posicion].enunciado}
+                            {Info[this.state.reto].descripcion}
                         </p>
-                        {((this.state.reto === 0 || this.state.reto === 2) ?
-                            <div className="row">
-                                <div className="col-md-2">
-                                </div>
-                                <div className="col-md-4" align="center">
-                                    <h2><strong>JAVASCRIPT</strong></h2>
-                                    <textarea id="text_js" className="Textarea"></textarea>
-                                </div>
-                                <div className="col-md-4" align="center">
-                                    <h2 ><strong>HTML</strong></h2>
-                                    <textarea id="text_html" className="Textarea"></textarea>
-                                </div>
-                                <div className="col-md-2">
+                        <div className="row">
+                            {
+                                (Info[this.state.reto].indexOf('js') ?
+                                    <div className="col-md-4" align="center">
+                                        <h2><strong>JAVASCRIPT</strong></h2>
+                                        <textarea id="text_js" className="Textarea"></textarea>
+                                    </div> : ''
+                                )
+                            }
+                            {
+                                (Info[this.state.reto].indexOf('css') ?
+                                    <div className="col-md-4" align="center">
+                                        <h2 ><strong>CSS</strong></h2>
+                                        <textarea id="text_css" className="Textarea"></textarea>
+                                    </div> : ''
+                                )
+                            }
+                            {
+                                (Info[this.state.reto].indexOf('html') ?
+                                    <div className="col-md-4" align="center">
+                                        <h2 ><strong>HTML</strong></h2>
+                                        <textarea id="text_html" className="Textarea"></textarea>
+                                    </div> : ''
+                                )
+                            }
 
-                                </div>
-                            </div>
-                            :
-                            <div className="row">
-                                <div className="col-md-2">
-                                </div>
-                                <div className="col-md-8" align="center">
-                                    <h2><strong>JAVASCRIPT</strong></h2>
-                                    <textarea id="text_js" className="Textarea"></textarea>
-                                </div>
-                                <div className="col-md-2">
-
-                                </div>
-                            </div>)}
+                        </div>
                         <div className="Space">
-                            <button type="button" onClick={this.sumar} id="Next">Next</button>
-                        </div> 
-                        <div className="Space">                     
-                            <Chulos chulosCompletados={this.state.reto} chulosTotales = {Info.length}/>                
-                        </div>   
+                            <button type="button" onClick={this.goNext} id="Next">Next</button>
+                        </div>
+                        <div className="Space">
+                            <Chulos chulosCompletados={this.state.reto} chulosTotales={Info.length} />
+                        </div>
                     </div>
                 </section>
             );
--- r-master/src/Components/CodingChallenge.js
+++ r-master/src/Components/CodingChallenge.js
@@ -2,46 +2,59 @@ import React, { Component } from 'react';
 import './Styles/Styles.css';
 import Challenge from './Challenge';
 import info from './Data/InfoComponent';
+import firebase from 'firebase';
+
 
 class CodingChallenge extends Component {
     constructor(props) {
         super(props);
         this.ChangeCom = this.ChangeCom.bind(this);
-        this.state = {id: true};
-      }
-      ChangeCom(){
-        this.setState({id: false});
-      }
-  render() {
-    const id = this.state.id;
+        this.state = { id: true, retos:[] };
+        this.retosArray=[];
+    }
+    ChangeCom() {
+        this.setState({ id: false });
+    }
+    componentWillMount() {
+        const rootRef = firebase.database().ref().child('Retos_info');
+
+        rootRef.on('child_added', (snapshot) => {
+            this.retosArray.push(snapshot.val());
+            this.setState({retos: this.retosArray});
+        });
+    }
+    render() {
+        const id = this.state.id;
 
 
-    if(id){
-    return (
-        <section className="Middle-block" id="challenge">         
-            <div>
-                <h3 className="h">
-                    CODING CHALLENGE
+        if (id) {
+            return (
+                <section className="Middle-block" id="challenge">
+                    <div>
+                        <h3 className="h">
+                            CODING CHALLENGE
                 </h3>
-                <p className="text" >
-                    We specialize in building the next generation of digital products leveraging the latest
+                        <p className="text" >
+                            We specialize in building the next generation of digital products leveraging the latest
                     UI technologies and architectures, multidevice techniques, big scale applications, component
                     based systems, intelligent user interfaces and the latest trends on user experience.
-                    <br/>
-                    <br/>
-                </p>                   
-            </div>
-            <button type="button" className="boton" id="start"  onClick={this.ChangeCom}>
-                Are you ready!
-            </button>   
-        </section>
-    )}else{
-    return(
-        <div>
-            <Challenge Posicion="0" Info={info}/>
-        </div>
-    )}
-  }
+                    <br />
+                            <br />
+                        </p>
+                    </div>
+                    <button type="button" className="boton" id="start" onClick={this.ChangeCom}>
+                        Are you ready!
+            </button>
+                </section>
+            )
+        } else {
+            return (
+                <div>
+                    <Challenge Info={this.state.retos} />
+                </div>
+            )
+        }
+    }
 }
 
 export default CodingChallenge;
